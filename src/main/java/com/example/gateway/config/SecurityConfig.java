b40c7ebb21a35386bfc04c109e817786f78cebd0
OrbitalMk <jguillen@minsa.gob.ni>
2024-02-20 12:52:16 -0600

    feat: add post-logout uri

--- src/main/java/com/example/gateway/config/SecurityConfig.java
+++ src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -3,11 +3,22 @@ package com.example.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
     
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -16,6 +27,16 @@ public class SecurityConfig {
                 .pathMatchers("/**").permitAll()
                 .anyExchange().authenticated())
             .oauth2Login(Customizer.withDefaults())
+            .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
             .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler = 
+            new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://127.0.0.1:8082");
+
+        return oidcLogoutSuccessHandler;
     }
 }
